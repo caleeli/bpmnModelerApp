e77668b1e47ac70e97c4b2efa1768338db79bda8
David Callizaya <davidcallizaya@gmail.com>
2019-01-07 15:20:57 -0400

    Update modeler and meta information

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
     pluginOptions: {
         electronBuilder: {
             builderOptions: {
+                "appId": "com.processmaker.modeler",
+                "mac": {
+                    "category": "public.app-category.developer-tools"
+                },
                 "icon": "src/assets/icons/processmaker.icns"
             }
         }
